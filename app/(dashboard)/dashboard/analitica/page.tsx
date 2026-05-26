3a636ee27d2f4b6a4d9e70a971db531811cd3358
José Mora <josemora@192.168.1.10>
2026-05-26 11:13:47 -0400

    Describe el cambio

--- app/(dashboard)/dashboard/analitica/page.tsx
+++ app/(dashboard)/dashboard/analitica/page.tsx
@@ -6,7 +6,7 @@ export default function AnalyticsPage() {
       <iframe
         width="100%"
         height="100%"
-        src="https://datastudio.google.com/embed/reporting/1fcc8b8a-6f42-40f9-b676-7485a6a4b247/page/s97ZF"
+        src="https://datastudio.google.com/embed/reporting/8e182061-ed03-45d1-beb5-b282e869f2c7/page/PlOzF"
         frameBorder="0"
         style={{ border: 0, display: "block" }}
         className="dark:brightness-[0.85] dark:contrast-[0.9] transition-[filter] duration-300"
