1c178e5bfb2130aea11a60b4f7457bd62294f170
José Mora <josemora@192.168.1.10>
2026-05-25 22:40:41 -0400

    Describe el cambio

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -6,7 +6,7 @@ export default function DashboardPage() {
     <div className="-m-6" style={{ overflow: "hidden", height: "calc(100vh - 64px)" }}>
       <iframe
         className="airtable-embed dark:brightness-[0.85] dark:contrast-[0.9] transition-[filter] duration-300"
-        src="https://airtable.com/embed/appZYamftetMYcboR/shr6zCa5GBjcMLSyT"
+        src="https://airtable.com/embed/appcg07cqX2rF6GU2/shr7rqAb120cso41g"
         frameBorder="0"
         width="100%"
         height="100%"
